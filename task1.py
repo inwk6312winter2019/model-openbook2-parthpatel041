c1ccb26f6005825c2095e0e3ed3cc225f353ce61
parthpatel041 <Parthkumar.Patel@dal.ca>
2019-02-06 12:44:25 -0800

    subtask 1 and2 completed subtask-3 remaining

--- task1.py
+++ task1.py
@@ -6,5 +6,16 @@ def tup():
        t = (v[2], v[4], v[6], v[7])
        print(t)
 
+def maintenance():
+    h = dict()
+    for f2 in f:
+       f2 = f2.split(",")
+       if f2[12] not in h:
+         h[f2[12]] = 1
+       else :
+         h[f[12]] += 1
+    print(h)
+
 tuple()
+maintenance()
 
