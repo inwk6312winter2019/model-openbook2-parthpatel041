d88bcf46b847afa5ab27dca556701cfbb637219e
parthpatel041 <Parthkumar.Patel@dal.ca>
2019-02-06 13:17:51 -0800

    subtask 1,2,3 completed

--- task1.py
+++ task1.py
@@ -12,10 +12,20 @@ def maintenance():
        f2 = f2.split(",")
        if f2[12] not in h:
          h[f2[12]] = 1
-       else :
+       else:
          h[f[12]] += 1
     print(h)
 
+def street_owner():
+    owner_list = f[11]
+    final_list = []
+    for a in f:
+        a = a.split(",")
+        if a[11] not in final_list:
+            final_list.append(a[11])
+    print(final_list)
+
 tuple()
 maintenance()
+street_owner()
 
